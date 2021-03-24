70cfc23f3dee6aede7ec9c2d02a3e9d6d84175d3
hzla <neohzla@gmail.com>
2021-03-24 00:56:02 -0700

    update installation bat

--- WINDOWS_INSTALL.bat
+++ WINDOWS_INSTALL.bat
@@ -1,5 +1,5 @@
-./installers/rubyinstaller-2.7.2-1-x64.exe
-./installers/python-3.9.2-amd64.exe
+rubyinstaller-2.7.2-1-x64.exe
+python-3.9.2-amd64.exe
 call bundle install
 pip install ndspy
 ECHO "Installation Successful, run Pokeweb_Windows.bat to start the server"
