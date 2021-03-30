07b4885f71bff999631efe875ebf2c7c5cc296c3
hzla <neohzla@gmail.com>
2021-03-30 11:00:48 -0700

    add random pallete option for alt forms

--- python/sprite_writer.py
+++ python/sprite_writer.py
@@ -8,6 +8,7 @@ import os
 import json
 import sys
 import copy
+import random
 
 
 def write_sprite_to_index(sprite_indexes, target_index):
@@ -24,8 +25,12 @@ def write_sprite_to_index(sprite_indexes, target_index):
 	form_count = 0
 	for sprite_index in sprite_indexes:
 		is_shiny = False
+		is_random = False
 		if sprite_index.split(" ")[0].lower() == "shiny":
 			is_shiny = True
+
+		if sprite_index.split(" ")[0].lower() == "rand":
+			is_random = True
 
 		sprite_index = int(sprite_index.split(" ")[-1]) 
 
@@ -38,6 +43,10 @@ def write_sprite_to_index(sprite_indexes, target_index):
 
 		if is_shiny:
 			to_copy[18] = to_copy[19]
+
+		if is_random:
+			rand_sprite = random.randint(1,649) + random.randint(0,1)
+			to_copy[18] = narc.files[(rand_sprite * 20) + 18]
 
 		narc.files[13700 + (20 * target_index): 13720 + (20 * target_index)] = to_copy
 
