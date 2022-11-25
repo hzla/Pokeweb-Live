d40b433e616767705da115d0f5b73f6f99941ffb
hzla <neohzla@gmail.com>
2022-11-24 23:59:42 -0800

    delete calc

--- public/click_index_html_to_run/calc/mechanics/gen3.d.ts
+++
@@ -1,6 +0,0 @@
-import { Generation } from '../data/interface';
-import { Pokemon } from '../pokemon';
-import { Move } from '../move';
-import { Field } from '../field';
-import { Result } from '../result';
-export declare function calculateADV(gen: Generation, attacker: Pokemon, defender: Pokemon, move: Move, field: Field): Result;
